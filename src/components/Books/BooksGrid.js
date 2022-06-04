8f60aa7a7af058041232788e7ad7f640e1a7c38a
xocarva <correo@xoancarneiro.com>
2022-06-04 09:58:03 +0200

    modify per page results

--- src/components/Books/BooksGrid.js
+++ src/components/Books/BooksGrid.js
@@ -19,7 +19,7 @@ const BooksGrid = ({ searchValue }) => {
     }, [ data, searchValue ]);
 
     const [ page, setPage ] = useState( 0 );
-    const perPage = 5;
+    const perPage = 7;
     const totalPages = Math.ceil( books?.length / perPage );
 
     return (
